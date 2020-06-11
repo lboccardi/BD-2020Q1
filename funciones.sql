754241a48283cd2adafff7984273ea8da20af85f
Lboccardi <lboccardi@itba.edu.ar>
2020-06-11 11:06:43 -0300

    added fillYear() and checkLeapYear()

--- funciones.sql
+++ funciones.sql
@@ -47,3 +47,35 @@ create table EVENT (
 	foreign key (Declaration_Date) references DATEDETAIL
 );
 
+create or replace function checkLeapYear(
+year int
+) returns boolean as $$
+    begin
+    return (year % 4 = 0) AND ((year % 100 <> 0) or (year % 400 = 0));
+    end;
+$$ language plpgsql;
+
+set datestyle to dmy;
+
+create or replace function fillYear (
+date Date
+) returns void as $$
+declare
+    year int := 0;
+    isleap bool := 0;
+    begin
+    year := extract (year from date);
+    isleap := checkLeapYear(year);
+    raise notice '% %', year, isleap;
+end;
+    $$ language plpgsql;
+
+DO $$
+ DECLARE
+ g date := '01/10/2020';
+ g2 date := '01/10/2019';
+ begin
+ perform fillYear(g);
+ perform fillYear(g2);
+end;
+$$
